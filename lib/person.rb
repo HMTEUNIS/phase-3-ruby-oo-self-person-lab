fb57ddde98b5820ba418f704684e5841c43d4143
Holly Teunis <+>
2021-11-29 21:03:42 -0500

    commit tree 0e6d67130492d61f893fe85f3f0f1ff367c1eaa7

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
-# your code goes here
+class Person
+    require "pry"
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygene
+    def initialize(name, bank_account = 25, happiness = 8, hygene = 8) 
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygene = hygene
+    end
+    def happiness_changes=(value)
+    if value.between? (0, 10)
+        @happiness = value
+    
+    elsif happiness
+     
+    end
+end
+
+   
+end
